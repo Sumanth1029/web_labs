cd036ded6ccd84b6d79a058192b8be9f296a466d
Sumanth Hegde <sumanthhegde1029@gmail.com>
2018-10-08 10:06:05 +0530

    Update count.php

--- count.php
+++ count.php
@@ -2,7 +2,7 @@
 	$fp =fopen("counter.txt","r");
 	$count=fread($fp,10);
 	fclose($fp);
-	$count=$count+10;
+	$count=$count+1;
 	echo "<p>Page views:".$count ."</p>";
 	$fp=fopen("counter.txt","w");
 	fwrite($fp,$count);
